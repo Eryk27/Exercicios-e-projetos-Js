18cfcc13d452f9d993c9bbef760b4bd1ba308850
Eryk27 <161753505+Eryk27@users.noreply.github.com>
2024-12-09 19:17:14 +0000

    oi

--- Flags js/js/main.js
+++ Flags js/js/main.js
@@ -1,1 +1,15 @@
 import flags from './model/flags.js';
+const main = document.querySelector('main');
+let flagsHTML = '';
+
+for (const flag of flags) {
+  flagsHTML += `
+    <div class="flag col-2 my-2 text-center">
+      <img src="${flag.image}" alt="${flag.name}">
+      <p>${flag.name}</p>
+    </div>
+  `;
+}
+
+
+main.innerHTML = flagsHTML;
